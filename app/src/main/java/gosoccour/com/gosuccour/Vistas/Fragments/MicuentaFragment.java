4aef3e3794afa237ed1d9e23625a660450a94a00
mohamed <mkamboua@jaumebalmes.net>
2018-05-23 19:03:21 +0200

    ultimo push

--- app/src/main/java/gosoccour/com/gosuccour/Vistas/Fragments/MicuentaFragment.java
+++ app/src/main/java/gosoccour/com/gosuccour/Vistas/Fragments/MicuentaFragment.java
@@ -1,7 +1,10 @@
 package gosoccour.com.gosuccour.Vistas.Fragments;
 
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,6 +17,8 @@ import gosoccour.com.gosuccour.R;
  */
 public class MicuentaFragment extends Fragment {
 
+    AppBarLayout appbar;
+    TabLayout tabs;
 
     public MicuentaFragment() {
         // Required empty public constructor
@@ -24,7 +29,25 @@ public class MicuentaFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_micuenta, container, false);
+        ViewGroup view = (ViewGroup) inflater.inflate(R.layout.fragment_micuenta, container, false);
+
+            if(savedInstanceState==null){
+                View parent =(View) container.getParent();
+                appbar = (AppBarLayout) parent.findViewById(R.id.appbar);
+                tabs = new TabLayout(getActivity());
+                tabs.setTabTextColors(Color.parseColor("#FFFFFF"), Color.parseColor("#FFFFFF"));
+                appbar.addView(tabs);
+            }
+
+
+        return view;
+    }
+
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        appbar.removeView(tabs);
     }
 
 }
